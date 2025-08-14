657ee9bcadefdcc88cffcb30e34f4bcdba9b9242
Michael Judge <j44dge@gmail.com>
2025-08-13 21:03:36 -0600

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    allowedHosts: ['test.j4dge.com'],
+    host: '0.0.0.0', // listen on all interfaces
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['test.j44dge.com'], // whitelist your hostname
   },
 })
