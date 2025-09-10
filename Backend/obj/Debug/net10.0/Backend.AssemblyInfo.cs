8a4df62c1ba032373e738dc03b8d784b50e210d5
Michael Judge <j44dge@gmail.com>
2025-09-09 22:21:14 -0600

    merge category work

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e50fc5e3da87a31c3c7d028b25c5414dbedc6c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f6c04dd0af482bffbed2ac16468fa9e6bd6a67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
